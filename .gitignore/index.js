91545e6f5e34af167ad5e6b709bf3a6e609fe963
freegan11210 <37715595+freegan11210@users.noreply.github.com>
2018-03-24 17:41:22 +0100

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js");
+
+var bot = Discod.Client(); 
+
+bot.on('ready', fonction() (
+    bot.user.setGame('lagonbot , !help');
+    console.log('Le bot a bien ete connecte');
+));
+
+
+bot.login('NDI3MTE3MjE4NzE5OTg5NzYx.DZf3nQ.bnyI8Ohd8SNZEPPrGVzlp9zIPGo');
